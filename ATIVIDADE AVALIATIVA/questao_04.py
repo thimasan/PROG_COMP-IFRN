0534db982c8158d607e67dd7f14ce6a29a5e188f
Thiago Santana <thyagus.santana@gmail.com>
2024-11-21 14:29:01 -0300

    DIAS TRANSCORRIDOS

--- ATIVIDADE AVALIATIVA/questao_04.py
+++ ATIVIDADE AVALIATIVA/questao_04.py
@@ -1,10 +1,5 @@
-'''O número de dias decorridos entre duas datas é importante em uma infinidade de situações da vida cotidiana.
-Faça um programa que recebe inicialmente dois valores (dia inicial e mês inicial), depois mais dois valores (dia final, mês final),
-ao final deve indicar quantos dias se passaram entre as datas (considere que o ano não é bissexto). 
-Exemplos:  o 02 de 05 até 03 de 05 - 1 dia o 27 de 04 até 03 de 05 - 6 dias o 03 de 02 até 03 de 05 - 79 dias 
-Não esqueça de verificar se a data inicial é menor ou igual à data final. 
-Dica: conte o número de dias até cada uma das datas e subtraia esses números.
-'''
+#                       DIAS TRANSCORRIDOS
+#---------------------------------------------------------------------------------------------------
 print('*'*60)
 # ENTRADA DOS DADOS E VALIDAÇÃO SIMULTÂNEA A CADA ENTRADA:
 
