7d9e8964be9924c5b9540c4b8e63ae91a34574f4
Thiago Santana <thyagus.santana@gmail.com>
2024-11-22 19:03:08 -0300

    update da aquisição de dados - versão anterior não funcionava corretamente. Novo método aplicado

--- ATIVIDADE AVALIATIVA/questao_06.py
+++ ATIVIDADE AVALIATIVA/questao_06.py
@@ -1,9 +1,9 @@
 #               AQUISIÇÃO DE DADOS
 
-strSexo = input('Informe seu Sexo: (Masculino/Feminino)')
-while strSexo != 'masculino' or strSexo != 'feminino':
-    print('Sexo inválido! Use Masculino ou Feminino.')
-    strSexo = input('Informe seu Sexo: (Masculino/Feminino)')
+strSexo = input('Informe seu Sexo (Masculino/Feminino):')
+while strSexo.lower() not in ["masculino","feminino"]:  
+    print('Sexo inválido! Use Masculino ou Feminino!')
+    strSexo = input('Informe seu Sexo (Masculino/Feminino):')
 
 data_nascimento = input('Informe sua data de nascimento (DD/MM/AAAA):')
 data_inic_contrib = input('Informe sua data de INICIO da contribuição Previdenciaria (DD/MM/AAAA):')
