5f7a82bb4a13909450375cc5debc4e3468c47946
Thiago Santana <thyagus.santana@gmail.com>
2024-11-24 22:45:21 -0300

    updade Cálculo da idade - calculo tempo de contrib. e separação dos calculos por tempo de contrib. e idade.

--- ATIVIDADE AVALIATIVA/questao_06.py
+++ ATIVIDADE AVALIATIVA/questao_06.py
@@ -13,27 +13,39 @@ data_nascimento = input('Informe sua data de nascimento (DD/MM/AAAA):')
 data_inic_contrib = input('Informe sua data de INICIO da contribuição Previdenciaria (DD/MM/AAAA):')
 
 
-#calculo_idade
+# CALCULO DA IDADE
 data_nascimento = parse(data_nascimento)
 data_atual = datetime.now()
 idade = relativedelta(data_atual, data_nascimento).years
 
-print(f'Você tem {idade} anos de idade!')
-
-#calculo_tempo_contribuicao
-
-
-
+# CALCULO TEMPO DE CONTRIBUIÇÃO
+data_inic_contrib = parse(data_inic_contrib)
+data_atual = datetime.now()
+tempo_contribuicao = relativedelta(data_atual, data_inic_contrib).years
 
 
-#               PROCESSAMENTO
+#   APOSENTADORIA POR IDADE
+
 if strSexo == 'masculino':
-    if idade >= 65 and tempo_contribuicao >= 35:
+    if idade >= 65 and tempo_contribuicao >= 15:
         print('Você já pode se aposentar!')
     else:
         print('Você ainda não pode se aposentar!')
 else:    
-    if idade >= 62 and tempo_contribuicao >= 30:
+    if idade >= 62 and tempo_contribuicao >= 15:
         print('Você já pode se aposentar!')
     else:
         print('Você ainda não pode se aposentar!')
+        print(f'Você tem {idade} anos de idade!')
+
+# APOSENTADORIA POR TEMPO DE CONTRIBUIÇÃO
+if strSexo == 'masculino':
+    if tempo_contribuicao >= 35:
+        print('Você já pode se aposentar por tempo de contribuição!')
+    else:
+        print('Você ainda não pode se aposentar por tempo de contribuição!')
+else:    
+    if tempo_contribuicao >= 30:
+        print('Você já pode se aposentar por tempo de contribuição!')
+    else:
+        print('Você ainda não pode se aposentar por tempo de contribuição!')
