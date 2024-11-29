9e2ca8798cff02f23afe06cef31b947dc825c7d8
thimasan <thyagus.santana@gmail.com>
2024-11-29 20:20:10 -0300

    correção da condição WHILE

--- AULA_06/atividade_02.py
+++ AULA_06/atividade_02.py
@@ -1,11 +1,9 @@
 # Fazer um programa que fique lendo números inteiros do usuário até que ele digite 0.
 # Quando o usuário digitar 0, o programa deve imprimir a soma de todos os números digitados bem como a média aritmética.
-
 numero = int(input("Digite um número qualquer : "))
-
 soma = 0
 contador = 0
-while true:
+while True:
     if numero == 0:
         break
     soma += numero
