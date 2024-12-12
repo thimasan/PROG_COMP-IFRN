50a87f8257573523276f037ec74c0c928acefe58
thimasan <thyagus.santana@gmail.com>
2024-12-11 21:37:12 -0300

    upload das atividades de laço for

--- AULA_09/Manip_strings_04.py
+++ AULA_09/Manip_strings_04.py
@@ -3,3 +3,13 @@
    Fazer um programa em que o usuário informe uma palavra e em seguida
    o programa exiba a palavra invertida e diga se ela é um palíndromo ou não.
 '''
+
+palavra = str(input("Digite uma palavra para verificar se é palíndromo:"))
+palindromo = ''
+for i in range(len(palavra)-1, -1, -1):
+    palindromo += palavra[i]
+
+if palavra.lower() == palindromo.lower():
+    print('É palindromo')
+else:
+    print('Não é palindromo')
--- AULA_09/Manip_strings_05.py
+++ AULA_09/Manip_strings_05.py
@@ -3,3 +3,16 @@
    Fazer um programa em que o usuário informe uma string e depois uma palavra
    e em seguida o programa informe se a palavra informada está na string informada
 '''
+
+palavra = str(input("Digite uma palavra"))
+frase = str(input("Digite uma frase ou string:"))
+
+palavras = frase.split()
+
+for palavra in palavras :
+    print('A palavra está na frase')
+
+if palavra.lower() == palindromo.lower():
+    print('É palindromo')
+else:
+    print('Não é palindromo')
