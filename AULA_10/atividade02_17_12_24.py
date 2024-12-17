ce7b647471ba8305aa39b572ac6e33791b0b32d1
thimasan <thyagus.santana@gmail.com>
2024-12-17 12:41:51 -0300

    upload atividade 02. Laço FOR 17/12/24

---
+++ AULA_10/atividade02_17_12_24.py
@@ -0,0 +1,29 @@
+'''
+   EXEMPLO 02:
+   Fazer um programa que solicite ao usuário valor inteiro positivo e em seguida
+   imprima conforme o exemplo abaixo:  
+
+   Exemplo:
+   Digite um valor inteiro: 5
+
+   1
+   1 2
+   1 2 3
+   1 2 3 4
+   1 2 3 4 5
+   1 2 3 4
+   1 2 3
+   1 2
+   1
+'''
+valor = int(input('Digite um número inteiro positivo:'))
+
+#laço
+for i in range(1, valor+1):
+    for j in range(1, i+1):
+        print(j, end=' ')
+    print()
+for i in range(valor-1, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=' ')
+    print()
