30722b5a185a19e35b543a6709ba6a144b8b610e
thimasan <thyagus.santana@gmail.com>
2025-01-28 20:14:10 -0300

    UPLOAD

---
+++ AULA_11/aula_22_01_25.py
@@ -0,0 +1,8 @@
+capitais_ne = ["Aracaju", "Salvador","Fortaleza","São Luís","Manaus","Recife","Natal",]
+
+
+capitais_ne.sort()
+
+for capital in capitais_ne:
+    print(capital)
+
---
+++ AULA_11/listas_06.py
@@ -0,0 +1,128 @@
+clubes = """Clube;Ano;V;E;D;GP;GC
+América Mineiro;2023;5;9;24;42;81
+América Mineiro;2022;15;8;15;40;40
+América Mineiro;2021;13;14;11;41;37
+Athletico Paranaense;2023;14;14;10;51;43
+Athletico Paranaense;2022;16;10;12;48;48
+Athletico Paranaense;2021;13;8;17;41;45
+Athletico Paranaense;2020;15;8;15;38;36
+Athletico Paranaense;2019;18;10;10;51;32
+Athletico-PR;2024;11;9;18;40;46
+Atlético Goianiense;2022;8;12;18;39;57
+Atlético Goianiense;2021;13;14;11;33;36
+Atlético Goianiense;2020;12;14;12;40;45
+Atlético Mineiro;2023;19;9;10;52;32
+Atlético Mineiro;2022;15;13;10;45;37
+Atlético Mineiro;2021;26;6;6;67;34
+Atlético Mineiro;2020;20;8;10;64;45
+Atlético Mineiro;2019;13;9;16;45;49
+Atlético-GO;2024;7;9;22;29;58
+Atlético-MG;2024;11;14;13;47;54
+Avaí;2022;9;8;21;34;60
+Avaí;2019;3;11;24;18;62
+Bahia;2024;15;8;15;49;49
+Bahia;2023;12;8;18;50;53
+Bahia;2021;11;10;17;42;51
+Bahia;2020;12;8;18;48;59
+Bahia;2019;12;13;13;44;43
+Botafogo;2024;23;10;5;59;29
+Botafogo;2023;18;10;10;58;37
+Botafogo;2022;15;8;15;41;43
+Botafogo;2020;5;12;21;32;62
+Botafogo;2019;13;4;21;31;45
+Ceará;2022;7;16;15;34;41
+Ceará;2021;11;17;10;39;38
+Ceará;2020;14;10;14;54;51
+Ceará;2019;10;9;19;36;41
+Chapecoense;2021;1;12;25;27;67
+Chapecoense;2019;7;11;20;31;52
+Corinthians;2024;15;11;12;54;45
+Corinthians;2023;12;14;12;47;48
+Corinthians;2022;18;11;9;44;36
+Corinthians;2021;15;12;11;40;36
+Corinthians;2020;13;12;13;45;45
+Corinthians;2019;14;14;10;42;34
+Coritiba;2023;8;6;24;41;73
+Coritiba;2022;12;6;20;39;60
+Coritiba;2020;7;10;21;31;54
+Criciúma;2024;9;11;18;42;61
+Cruzeiro;2024;14;10;14;43;41
+Cruzeiro;2023;11;14;13;35;32
+Cruzeiro;2019;7;15;16;27;46
+CSA;2019;8;8;22;24;58
+Cuiabá;2024;6;12;20;29;49
+Cuiabá;2023;14;9;15;40;39
+Cuiabá;2022;10;11;17;31;42
+Cuiabá;2021;10;17;11;34;37
+Flamengo;2024;20;10;8;61;42
+Flamengo;2023;19;9;10;56;42
+Flamengo;2022;18;8;12;60;39
+Flamengo;2021;21;8;9;69;36
+Flamengo;2020;21;8;9;68;48
+Flamengo;2019;28;6;4;86;37
+Fluminense;2024;12;10;16;33;39
+Fluminense;2023;16;8;14;51;47
+Fluminense;2022;21;7;10;63;41
+Fluminense;2021;15;9;14;38;38
+Fluminense;2020;18;10;10;55;42
+Fluminense;2019;12;10;16;38;46
+Fortaleza;2024;19;11;8;53;39
+Fortaleza;2023;15;9;14;45;44
+Fortaleza;2022;15;10;13;46;39
+Fortaleza;2021;17;7;14;44;45
+Fortaleza;2020;10;11;17;34;44
+Fortaleza;2019;15;8;15;50;49
+Goiás;2023;9;11;18;36;53
+Goiás;2022;11;13;14;40;53
+Goiás;2020;9;10;19;41;63
+Goiás;2019;15;7;16;46;64
+Grêmio;2024;12;9;17;44;50
+Grêmio;2023;21;5;12;63;56
+Grêmio;2021;12;7;19;44;51
+Grêmio;2020;14;17;7;53;40
+Grêmio;2019;19;8;11;64;39
+Internacional;2024;18;11;9;53;36
+Internacional;2023;15;10;13;46;45
+Internacional;2022;20;13;5;58;31
+Internacional;2021;12;12;14;44;42
+Internacional;2020;20;10;8;61;35
+Internacional;2019;16;9;13;44;39
+Juventude;2024;11;12;15;48;59
+Juventude;2022;3;13;22;29;69
+Juventude;2021;11;13;14;36;44
+Palmeiras;2024;22;7;9;60;33
+Palmeiras;2023;20;10;8;64;33
+Palmeiras;2022;23;12;3;66;27
+Palmeiras;2021;20;6;12;58;43
+Palmeiras;2020;15;13;10;51;37
+Palmeiras;2019;21;11;6;61;32
+Red Bull Bragantino;2024;10;14;14;44;48
+Red Bull Bragantino;2023;17;11;10;49;35
+Red Bull Bragantino;2022;11;11;16;49;59
+Red Bull Bragantino;2021;14;14;10;55;46
+Red Bull Bragantino;2020;13;14;11;50;40
+Santos;2023;11;10;17;39;64
+Santos;2022;12;11;15;44;41
+Santos;2021;12;14;12;35;40
+Santos;2020;14;12;12;52;51
+Santos;2019;22;8;8;60;33
+São Paulo;2024;17;8;13;53;43
+São Paulo;2023;14;11;13;40;38
+São Paulo;2022;13;15;10;55;42
+São Paulo;2021;11;15;12;31;39
+São Paulo;2020;18;12;8;59;41
+São Paulo;2019;17;12;9;39;30
+Sport;2021;9;11;18;24;37
+Sport;2020;12;6;20;31;50
+Vasco da Gama;2024;14;8;16;43;56
+Vasco da Gama;2023;12;9;17;41;51
+Vasco da Gama;2020;10;11;17;37;56
+Vasco da Gama;2019;12;13;13;39;45
+Vitória;2024;13;8;17;45;52"""
+
+
+lstClubes = list()
+
+lstClubes = [linha.split(';') for linha in clubes.strip().split('\n')[1:]]
+print(lstClubes [5:])
+
